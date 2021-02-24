889bc8372b87bff331f19891ec3241cc0a16d523
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-24 16:04:21 +0530

    Added :inserted 30 in between 56 and 70

--- LinkList/src/test/java/com/Link/LinkList/LinkedListTest.java
+++ LinkList/src/test/java/com/Link/LinkList/LinkedListTest.java
@@ -11,14 +11,32 @@ import org.junit.Test;
 public class LinkedListTest 
 {
 	LinkedList linkedList; 
-   
+    
 	
 	@Before
 	public void init() {
 		linkedList = new LinkedList();
 	}
 	
-	    
+	
+    @Test
+    public void addElementsAtFirst_Correct(){
+    	linkedList.addNodeAtFirst(70);
+    	linkedList.addNodeAtFirst(30);
+    	linkedList.addNodeAtFirst(56);
+        assertEquals(3, linkedList.getSize());
+        assertEquals(56, linkedList.returnFirst());
+    }
+    
+    @Test
+    public void addElementsAtFirst_InCorrect(){
+    	linkedList.addNodeAtFirst(70);
+    	linkedList.addNodeAtFirst(30);
+    	linkedList.addNodeAtFirst(56);
+        assertEquals(3, linkedList.getSize());
+        assertNotEquals(70, linkedList.returnFirst());
+    }
+    
     @Test
     public void addElementsAtLast_Correct(){
     	linkedList.addNodeAtLast(56);
@@ -33,6 +51,24 @@ public class LinkedListTest
     	linkedList.addNodeAtLast(56);
     	linkedList.addNodeAtLast(30);
     	linkedList.addNodeAtLast(70);
+        assertEquals(3, linkedList.getSize());
+        assertNotEquals(70, linkedList.returnFirst());
+    }
+    
+    @Test
+    public void addElementsAtIndex_Correct(){
+    	linkedList.addNodeAtLast(56);
+    	linkedList.addNodeAtLast(70);
+    	linkedList.addNodeAtIndex(30);
+        assertEquals(3, linkedList.getSize());
+        assertEquals(56, linkedList.returnFirst());
+    }
+    
+    @Test
+    public void addElementsAtIndex_InCorrect(){
+    	linkedList.addNodeAtLast(56);
+    	linkedList.addNodeAtLast(70);
+    	linkedList.addNodeAtIndex(30);
         assertEquals(3, linkedList.getSize());
         assertNotEquals(70, linkedList.returnFirst());
     }
