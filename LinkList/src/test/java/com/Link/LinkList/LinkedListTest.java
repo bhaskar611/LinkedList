4cc04abfa3cc0361d40033b0732d295fc1a5329d
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-24 15:16:13 +0530

    Added: Appended 30 and 70 to 56

--- LinkList/src/test/java/com/Link/LinkList/LinkedListTest.java
+++ LinkList/src/test/java/com/Link/LinkList/LinkedListTest.java
@@ -1,6 +1,7 @@
 package com.Link.LinkList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -10,20 +11,29 @@ import org.junit.Test;
 public class LinkedListTest 
 {
 	LinkedList linkedList; 
-    
+   
 	
 	@Before
 	public void init() {
 		linkedList = new LinkedList();
 	}
 	
-	
+	    
     @Test
-    public void addElementsAtFirst(){
-    	linkedList.addNodeAtFirst(70);
-    	linkedList.addNodeAtFirst(30);
-    	linkedList.addNodeAtFirst(56);
-        assertEquals(3, linkedList.size);
+    public void addElementsAtLast_Correct(){
+    	linkedList.addNodeAtLast(56);
+    	linkedList.addNodeAtLast(30);
+    	linkedList.addNodeAtLast(70);
+        assertEquals(3, linkedList.getSize());
         assertEquals(56, linkedList.returnFirst());
+    }
+    
+    @Test
+    public void addElementsAtLast_InCorrect(){
+    	linkedList.addNodeAtLast(56);
+    	linkedList.addNodeAtLast(30);
+    	linkedList.addNodeAtLast(70);
+        assertEquals(3, linkedList.getSize());
+        assertNotEquals(70, linkedList.returnFirst());
     }
 }
