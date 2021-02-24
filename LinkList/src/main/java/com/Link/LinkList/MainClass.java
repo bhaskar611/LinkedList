e4f58b7ade451bc32125d06df965ddb85941149d
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-24 14:16:57 +0530

    Added: welcome message

---
+++ LinkList/src/main/java/com/Link/LinkList/MainClass.java
@@ -0,0 +1,9 @@
+package com.Link.LinkList;
+
+public class MainClass {
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Welcome to Linked List Operations!" );
+	       
+	    }
+}
