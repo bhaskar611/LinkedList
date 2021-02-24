4b628002f0aeecaa328fdd1ea4129e74bc125f10
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-24 14:58:21 +0530

    Added:added 30 and 56 to 70 in linklist

--- LinkList/src/main/java/com/Link/LinkList/MainClass.java
+++ LinkList/src/main/java/com/Link/LinkList/MainClass.java
@@ -1,9 +1,14 @@
 package com.Link.LinkList;
 
-public class MainClass {
-	 public static void main( String[] args )
-	    {
-	        System.out.println( "Welcome to Linked List Operations!" );
-	       
-	    }
+public class MainClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Linked List Problem" );
+        LinkedList linkedList = new LinkedList();
+        linkedList.addNodeAtFirst(70);
+        linkedList.addNodeAtFirst(30);
+        linkedList.addNodeAtFirst(56);
+        linkedList.printList();
+    }
 }
